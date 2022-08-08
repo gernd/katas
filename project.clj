7be1a75918b2cc8f3c60cca88073ba3d3ac138a5
Gernot Pointner <gernot.pointner@jambit.com>
2022-08-08 19:28:09 +0200

    Update clojure version and adapt description

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject katas "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A project containing code katas"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.trace "0.7.11"]]
   :plugins [[lein-cljfmt "0.6.6"]
             [lein-ancient "0.6.15"]]
