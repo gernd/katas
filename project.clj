9b0b3cf01597d895a9c302745d177794f1334f56
Gernot Pointner <gernot.pointner@jambit.com>
2022-12-02 16:39:26 +0100

    Add function for reading strings collections separated by newlines

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.trace "0.7.11"]]
+                 [org.clojure/tools.trace "0.7.11"]
+                 [djblue/portal "0.34.2"]]
   :plugins [[lein-cljfmt "0.6.6"]
             [lein-ancient "0.6.15"]]
   :repl-options {:init-ns katas.core})
